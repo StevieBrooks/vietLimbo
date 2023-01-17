5eb502ba698affbf4da002683e368230d7c5be2d
Steven Brooks <brookscodeblog@gmail.com>
2023-01-17 15:45:47 +0700

    Hamburger menu display commit

--- vLScript.js
+++ vLScript.js
@@ -1,7 +1,11 @@
-// MAIN-HEADER
-const mainLogo = document.querySelector('.main-header .logo');
-const mainNav = document.querySelector('.main-nav');
-mainLogo.addEventListener('touch', touchLogo);
-function touchLogo() {
-    mainNav.style.display = 'flex';
+// BURGER MENU
+const mainNavBurger = document.querySelector('.main-nav-burger');
+const burgerSlice = document.querySelectorAll('.burger-slice');
+
+mainNavBurger.addEventListener('click', mainNavFunc);
+function mainNavFunc() {
+    burgerSlice.forEach(function(el) {
+        el.style.display = 'none';
+    });
+    mainNavBurger.children[0].style.display = 'block';
 }
