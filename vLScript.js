a4b4c7134f13d6ea3e5bdba26ece25432ea1628f
Steven Brooks <brookscodeblog@gmail.com>
2023-01-18 16:26:39 +0700

    Add overlay for hamburger menu

--- vLScript.js
+++ vLScript.js
@@ -2,8 +2,10 @@
 const mainNavBurger = document.querySelector('.main-nav-burger');
 const bar = document.querySelectorAll('.bar');
 const mainNavList = document.querySelector('.main-nav-list');
+const overlay = document.querySelector('.overlay');
 
 mainNavBurger.addEventListener('click', () => {
     mainNavBurger.classList.toggle("active");
     mainNavList.classList.toggle("active");
+    overlay.classList.toggle("active");
 })
