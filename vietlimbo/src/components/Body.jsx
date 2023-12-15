faab8854246292763c2afcb1cb0bc9a3d8ca6b21
StevieBrooks <brookscodeblog@gmail.com>
2023-12-15 14:28:44 +0700

    social proof and resources styling

--- vietlimbo/src/components/Body.jsx
+++ vietlimbo/src/components/Body.jsx
@@ -2,28 +2,29 @@ import hero1 from "../images/hero1.jpg"
 import hero2 from "../images/hero2.jpg"
 import hero3 from "../images/hero3.jpg"
 import calendar from "../images/calendar.png"
+import avatar from "../images/lily1.png"
 
 export default function Body() {
 
     const studentReviews = [
         {
             name: "Qianni H",
-            rating: "10/10",
+            rating: "5/5",
             review: "Lily is a very good, patient, knowledgeable, and fun teacher! I learned a lot from her. Recommend!",
         }, 
         {
             name: "InfinityMadison",
-            rating: "10/10",
+            rating: "5/5",
             review: "Cam on for today's class Lim",
         }, 
         {
             name: "Gözde K",
-            rating: "10/10",
+            rating: "5/5",
             review: "Our first lesson was very informative and organised. Lim seems very experienced in her field and has a very friendly and warm approach. Looking forward to continue! ",
         }, 
         {
             name: "alci720",
-            rating: "10/10",
+            rating: "5/5",
             review: "Who's the best? - Lily!",
         }, 
     ]
@@ -31,7 +32,7 @@ export default function Body() {
     return (<>
     
         <section className="hero bg-primary px-5 pt-[10rem] pb-[15rem]">
-            <div className="hero-container bg-secondary m-5 p-3 max-w-[80rem] mx-auto">
+            <div className="hero-container bg-secondary m-5 p-3 max-w-[80rem] mx-auto border-4 border-secblack rounded-xl">
                 <h2 className=" text-white my-5 px-5 tracking-wide">TAKING THE BABY STEPS TO BECOME A MASTER VIETNAMESE SPEAKER</h2>
                 <p className=" text-white text-lg mb-5 p-5">Hello and the warmest of welcomes to Viet Limbo. Let me begin by saying that I'm ecstatic to accommodate you here and sincerely hope that we can grow together. By coming here, you are already helping me, therefore, I feel it is my duty and my honour to go above and beyond to help you.</p>
 
@@ -71,33 +72,47 @@ export default function Body() {
             </div>
         </section>
 
-        <section className="social-proof">
+        <section className="social-proof border-t-8 border-primary py-5">
             <h2 className="bg-white text-center py-5">Still not sold? My students are overjoyed!</h2>
-            <p className="bg-white text-center py-5 text-xl">I'm not one to blow my own trumpet, so I've dropped a few reviews from past and present students below...</p>
+            <p className="bg-white text-center pt-3 pb-[2.5rem] px-2 text-xl border-b-8 border-primary">I'm not one to blow my own trumpet, so I've dropped a few reviews from past and present students below...</p>
+
             <div className="review-container">
 
                 {studentReviews.map((item, index) => (
-                    <div key={index} className={`review-card bg-secondary text-white p-5 my-[3rem] ${index % 2 == 0 ? "mx-auto" : "ms-10"} w-3/5 border-2 border-secblack rounded-lg hover:-translate-y-0.5 transition duration-300`}>
-                        <h3 className="student-name">{item.name}</h3>
-                        <span className="student-rating">{item.rating}</span>
-                        <p className="student-review text-white">❝{item.review}❞</p>
+                    <div key={index} className={`review-card bg-secondary text-white p-5 my-[5rem] ${index % 2 == 0 ? "mx-auto" : "ms-10"} w-3/5 border-4 border-secblack rounded-lg hover:-translate-y-0.5 transition duration-300`}>
+                        <span className="student-rating bg-primary p-5 border-4 border-secblack rounded-full relative bottom-9 font-bold text-2xl text-secblack tracking-widest">{item.rating}</span>
+                        <p className="student-review text-white text-xl text-center px-1 py-3">❝ {item.review} ❞</p>
+                        <h3 className="student-name text-md"><i>- {item.name}</i></h3>
                     </div>
                 ))}
 
             </div>
         </section>
 
-        <section className="resources-cta">
+        <section className="resources-cta bg-primary p-5 pb-[2rem]">
             <div className="resources-container">
                 <div className="img-container">
                     <img src="" alt="" />
                 </div>
                 <div className="text-container">
-                    <h2>Check out some of these free resources</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi necessitatibus et vero, nulla mollitia facilis cupiditate quasi perferendis officiis quisquam.</p>
+                    <h2 className="text-white py-5">Check out some of these free resources</h2>
+                    <p className="text-white text-lg py-5 px-2">For now, the resources on this site are limited. However, as we grow, both the quality and consistency is expected to improve so we wholeheartedly welcome your support in the meantime. Moreover, should you have any suggestions for us, we are more than happy to hear them. Please feel free to hit us up on the Contact page. For now, these alphabet cards should be enough to get you started.</p>
                 </div>
-                <div className="button-container">
-                    <button>View Resources!</button>
+                <div className="button-container text-right">
+                    <button className="border-2 border-secblack bg-white hover:bg-secondary text-lg font-bold px-5 py-2 me-[5rem] my-5 rounded-md hover:text-white">View Resources!</button>
+                </div>
+            </div>
+        </section>
+
+        <section className="about-intro">
+            <div className="intro-container">
+                <div className="img-container">
+                    <img src={avatar} alt="" className="w-80" />
+                </div>
+                <div className="text-container">
+                    <h2>So you can put a face to the name</h2>
+                    <p>As Viet Limbo is still in its infancy, please don't be surprised or demotivated if a few teething problems occur. My husband and I are hard at work to build a unique and valuable ensemble of resources to aid you learn Vietnamese. As you may be aware already, Vietnamese pronunciation is no easy feat. So I would like to invite you to check out the resources tab in the main menu. The Viet Limbo team - well, my hubby and I - have put together some vocal cards to help you master the correct pronunciation of the Vietnamese alphabet.</p>
+                    <p>My husband is currently training to be a software developer and as his skillset grows, so will the complexity and value of this site. It is our humble ambition to develop one of the finest resources of Vietnamese language education on the market. Incidentally, should you have any suggestions or criticisms, I would be more than delighted to hear them. For now, please remember that learning this melifluous language is not a sprint, it's a marathon. Take your time and I hope you enjoy this journey as much as I do. Please don't hesitate to reach out if you need anything. Your friend and Vietnamese mentor, Lily.</p>
                 </div>
             </div>
         </section>
