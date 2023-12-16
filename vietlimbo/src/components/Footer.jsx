79e7e5db1446aa33aa806e6d06966d4d4d402e9e
StevieBrooks <brookscodeblog@gmail.com>
2023-12-16 10:14:50 +0700

    footer basic styling, not responsive yet

--- vietlimbo/src/components/Footer.jsx
+++ vietlimbo/src/components/Footer.jsx
@@ -1,17 +1,20 @@
 export default function Footer() {
     return (<>
     
-        <footer className="flex justify-between bg-primary">
-            <div className="brand">
-                <h3>VietLimbo</h3>
-                <p>Copyright 2024</p>
+        <footer className="flex justify-between items-center bg-primary py-5">
+            <div className="brand-container">
+            <div className="brand flex items-center ms-3 cursor-pointer">
+                <span className="logo bg-white text-secblack text-2xl font-bold font-serif px-1 rounded border-2 shadow-md">V</span>
+                <h1 className="ps-1 text-secblack text-md hover:text-white underline underline-offset-4">ietLimbo</h1>
+            </div>
+                <p className="ms-3 text-center text-lg">Copyright 2024</p>
             </div>
 
-            <div className="footer-menu">
-                <ul className="flex">
-                    <li>About</li>
-                    <li>Privacy</li>
-                    <li>Contact</li>
+            <div className="footer-menu w-4/5">
+                <ul className="flex justify-between mx-5 text-lg">
+                    <li className="hover:text-white"><a href="">About</a></li>
+                    <li className="hover:text-white"><a href="">Privacy</a></li>
+                    <li className="hover:text-white"><a href="">Contact</a></li>
                 </ul>
             </div>
         </footer>
