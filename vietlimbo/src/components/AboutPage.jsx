604268c183afce0653c4db058adc17d157a7296a
StevieBrooks <brookscodeblog@gmail.com>
2023-12-18 13:42:29 +0700

    do footer links and About page content

--- vietlimbo/src/components/AboutPage.jsx
+++ vietlimbo/src/components/AboutPage.jsx
@@ -3,8 +3,38 @@ import BodyContext from "../contextFile"
 
 export default function AboutPage() {
     return (<>
+
+        <section className="hero bg-primary px-5 pt-[10rem] pb-[15rem]">
+            <div className="hero-container bg-secondary m-5 p-3 max-w-[80rem] mx-auto border-4 border-secblack rounded-xl">
+                <img src="" alt="" />
+            </div>
+        </section>
     
-        <p className="mt-[10rem]">This is the About page</p>
+        <h2>Viet Limbo: Your online guide to Vietnamese</h2>
+        <p>Being a native English speaker trying to study Vietnamese is not an easy task. Running a marathon with a baby elephant in tow would be easier. In terms of the language, the first thing that foreigners struggle with is the pronunciation. Vietnamese phonics have proven to be a huge obstacle for the majority of foreigners for eons. Naturally, many quit and simply enjoy Vietnam for the wonderful country that it is.</p>
+
+
+        <p>Neglecting to learn the language may be to the detriment of visitors. On the other hand, Vietnamese language sounds mellifluous either way. There is something truly blissful and satisfying about the way Tiếng Việt (Vietnamese language) rolls off the tongue. While difficult to master, it is not impossible.</p>
+
+        <p>Not to discourage you, but Vietnamese is generally regarded as one of the toughest languages for native English speakers to grasp. Therefore, it may come as no surprise that locals are encouraging and supportive towards anyone trying to study Vietnamese.</p>
+
+        <p>Once you’ve got past “hello” and “thank you” and the basic pronouns, you’re onto a winner. Making the effort to communicate with locals will not go unrewarded. At the very least, you can expect a smile and some friendly banter. Albeit, it might be a while before you understand exactly what is being said.</p>
+
+        <h2>“Study Vietnamese”, she said. Well then, help!</h2>
+
+        <p>Viet Limbo was founded in 2023 by Steven and Lily who’ve been an item since 2016. In short, the westerner came to Vietnam and fell in love with a local girl. Lily is from the mountains and she’s something of a country bumpkin. On the other hand, Steven is from Manchester and was more familiar with city life. Opposites attract…match made in heaven, right?</p>
+
+        <p>Wrong. Steven has a tendency to act like a primate and that drives Lily round the bend. On the flip side, Lily enjoys her afternoon naps and spends too much time on Facebook. This gives Steven plenty to moan about too. When this barmy pair aren’t squabbling, they work hard in the field of education. Lily has been teaching Vietnamese online since 2016, but she prefers to wind Steven up in English. Steven has also worked as a teacher for too many tears…ahem, years. In June 2022, he decided to make a career shift into web development and this website happens to be his first brainfart.</p>
+
+        <p>With Lily’s invaluable support, the pair of them hope to develop an online resource to introduce you to the wonderful Vietnam. Steven is currently striving to learn the language. Meanwhile, Lily is currently striving to avoid teaching him directly. However, their mission is to build an impeccable and benevolent resource together. Using a playful blend of imagination, local insight, and over a decade of teaching experience, Viet Limbo aims to be the creme-de-la-creme.</p>
+
+        <p>Steven has tried to study Vietnamese numerous times. Unfortunately, every effort has failed so far. These failures have been for multiple reasons including lack of motivation or using inconsistent resources. However, his attempts have not been in vain. Thanks to his interest in coding, and with Lily’s help, a truly original resource is on the horizon.</p>
+
+        <h2>Study Vietnamese With Something Completely Different</h2>
+
+        <p>Eventually, this website will include everything you need to feel right at home in Vietnam. Simple flashcard activities to help you master the basic concepts. Intriguing games and exercises to make practicing more fun. With Viet Limbo, you can study Vietnamese in a witty and light-hearted environment. Every aspect of the language will be covered and encouragement will be in full swing.</p>
+
+        <p>Viet Limbo also aims to provide an invaluable and unique insight into life in this beautiful country. With Lily as your benevolent guide, you are equipped with only the best. Moreover, Steven happens to be in the same position as you. As this site grows, so will his aptitude for the language and customs, as well as his love for Lily and her home country.</p>
 
     </>)
 }
--- vietlimbo/src/components/Footer.jsx
+++ vietlimbo/src/components/Footer.jsx
@@ -1,20 +1,51 @@
+import { useContext } from "react"
+import BodyContext from "../contextFile"
+
 export default function Footer() {
+
+    const [pageActive, setPageActive]= useContext(BodyContext)
+
+    const brandFunction = (e) => {
+        e.preventDefault()
+        setPageActive("body")
+    }
+
+    const aboutFunction = (e) => {
+        e.preventDefault()
+        setPageActive("about")
+    }
+
+    const servicesFunction = (e) => {
+        e.preventDefault()
+        setPageActive("services")
+    }
+
+    const resourcesFunction = (e) => {
+        e.preventDefault()
+        setPageActive("resources")
+    }
+
+    const contactFunction = (e) => {
+        e.preventDefault()
+        setPageActive("contact")
+    }
+
     return (<>
     
         <footer className="flex justify-between items-center bg-primary py-5">
             <div className="brand-container">
-            <div className="brand flex items-center ms-3 cursor-pointer">
-                <span className="logo bg-white text-secblack text-2xl font-bold font-serif px-1 rounded border-2 shadow-md">V</span>
-                <h1 className="ps-1 text-secblack text-md hover:text-white underline underline-offset-4">ietLimbo</h1>
-            </div>
+                <div className="brand flex items-center ms-3 cursor-pointer" onClick={brandFunction}>
+                    <span className="logo bg-white text-secblack text-2xl font-bold font-serif px-1 rounded border-2 shadow-md">V</span>
+                    <h1 className="ps-1 text-secblack text-md hover:text-white underline underline-offset-4">ietLimbo</h1>
+                </div>
                 <p className="ms-3 text-center text-lg">Copyright 2024</p>
             </div>
 
             <div className="footer-menu w-4/5">
                 <ul className="flex justify-between mx-5 text-lg">
-                    <li className="hover:text-white"><a href="">About</a></li>
+                    <li className="hover:text-white"><a href="" onClick={aboutFunction}>About</a></li>
                     <li className="hover:text-white"><a href="">Privacy</a></li>
-                    <li className="hover:text-white"><a href="">Contact</a></li>
+                    <li className="hover:text-white" onClick={contactFunction}><a href="">Contact</a></li>
                 </ul>
             </div>
         </footer>
